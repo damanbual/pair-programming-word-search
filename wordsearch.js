0a729568c3e4f0376e156ee20a3334eb9614731e
Daman Bual <damanbual@gmail.com>
2024-06-17 14:55:24 -0700

    added word search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,23 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  if (letters.length === 0) return false;
+
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  
+  const numRows = letters.length;
+  const numCols = letters[0].length;
+    
+  for (let col = 0; col < numCols; col++) {
+    let verticalJoin = '';
+    for (let row = 0; row < numRows; row++) {
+      verticalJoin += letters[row][col];
+    }
+    if (verticalJoin.includes(word)) return true;
+  }
+  return false;
+};
+
+module.exports = wordSearch;
